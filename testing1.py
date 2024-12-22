d0f26c00b05169698681d3bc75ba329c5f0b71f9
shahid-abdul <abdulshahidsyed7862@gmail.com>
2024-12-22 10:44:35 +0530

    exception handling

--- testing1.py
+++ testing1.py
@@ -20,19 +20,29 @@ lst = [5+8j,'Abdul',5.5,30,10,2.0,1+2j,20,3.5,'Tharun']
 # s=[i for i in lst if isinstance(i,int)]+[i for i in lst if isinstance(i,float)]+[i for i in lst if isinstance(i,complex)]+[i for i in lst if isinstance(i,str)]
 # print(s)
 
-class myclass:
-    def method(self,a):
-        print('method 1')
-        print(a)
+# class myclass:
+#     def method(self,a):
+#         print('method 1')
+#         print(a)
 
-    def method(self,a,b):
-        print('method 2')
-        print(a,b ,end=" ")
+#     def method(self,a,b):
+#         print('method 2')
+#         print(a,b ,end=" ")
 
-    def method(self,a,b,c):
-        print('method 3')
-        print(a,b,c,end=" ")
+#     def method(self,a,b,c):
+#         print('method 3')
+#         print(a,b,c,end=" ")
 
-m1 = myclass()
-m1.method(1,2,3)
-m1.method(1,2)
+# m1 = myclass()
+# m1.method(1,2,3)
+# m1.method(1,2)
+
+# s1 = input()
+# s2 = ''
+# # count = 0
+# for i in s1:
+#     if s1.count(i)>1:
+#         if i not in s2:
+#             s2 += i
+# print(s2)
+
